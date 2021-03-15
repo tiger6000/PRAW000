de088447cbe52998eeaaf916d1d1a84fb4ebbc27
aysi4ever <sfaysi2020@gmail.com>
2021-03-15 03:03:23 -0700

    so it works now this prints the id of each post on the front page.

--- main.py
+++ main.py
@@ -5,42 +5,35 @@
 import requests
 import requests.auth
 import praw
+import json
+from praw.models import MoreComments
+#   How to debug PRAW auth comments:
+#   from bboe Perhaps your authentication isn't set up properly.
+# What does print(reddit.user.me()) result in? If you get an exception you haven't successfully authenticated.
 
-
-def grantToken():
-    client_auth = requests.auth.HTTPBasicAuth('66AfkXHprP5E5Q', 'U19IWBHU4Ukg5onuqRBU3JbJlsdoXA')
-    post_data = {"grant_type": "password", "username": "CandyEnvironmental",
-                 "password": "_Qa]Lk_sRF8Slr=>RolO>)Bqn&>_x%sL00An-H#?dY3K4zvEs1WqA7CEUyMU&kdr"}
-    headers = {"User-Agent": "ChangeMeClient/0.1 by CandyEnvironmental"}
-    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data,
-                             headers=headers)
-    # Press the green button in the gutter to run the script.
-    jsonObject = response.json()
-    accessToken = jsonObject['access_token']
-    print(jsonObject)
-    #return accessToken
-
-def testToken(accessToken):
-    headers = {"Authorization": "bearer 546149535203-_dEA-knoVFOgTnbbxsF671lkbPDC_Q",
-               "User-Agent": "ChangeMeClient/0.1 by CandyEnvironmental"}
-    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-    jsonObject = response.json()
-    print(jsonObject)
-
-def connectToPRAW():
-    reddit = praw.Reddit(
-        client_id="66AfkXHprP5E5Q",
-        client_secret="546149535203-_dEA-knoVFOgTnbbxsF671lkbPDC_Q",
-        user_agent="PRAW000/0.1 by CandyEnvironmental - just checking out the api!",
-        username="CandyEnvironmental",
-        password="_Qa]Lk_sRF8Slr=>RolO>)Bqn&>_x%sL00An-H#?dY3K4zvEs1WqA7CEUyMU&kdr",
-    )
-    print(reddit.get())
 
 if __name__ == '__main__':
-    #grantToken()
-    #testToken()
-    connectToPRAW()
+    client_auth = requests.auth.HTTPBasicAuth('66AfkXHprP5E5Q', 'U19IWBHU4Ukg5onuqRBU3JbJlsdoXA')
+    post_data = {"grant_type": "password", "username": "CandyEnvironmental", "password": "_Qa]Lk_sRF8Slr=>RolO>)Bqn&>_x%sL00An-H#?dY3K4zvEs1WqA7CEUyMU&kdr"}
+    headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    reddit = response.json()
+
+
+    reddit = praw.Reddit(
+        client_id="66AfkXHprP5E5Q",
+        client_secret="U19IWBHU4Ukg5onuqRBU3JbJlsdoXA",
+        password="_Qa]Lk_sRF8Slr=>RolO>)Bqn&>_x%sL00An-H#?dY3K4zvEs1WqA7CEUyMU&kdr",
+        user_agent="USERAGENT",
+        username="CandyEnvironmental",
+    )
+    for submission in reddit.front.hot():
+        print(submission)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    # headers = {"Authorization": "bearer 546149535203-ebZe4rP46dQfnebQB3NWXbNLqKFphw",
+    #            "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    # response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+    # reddit = response.json()
